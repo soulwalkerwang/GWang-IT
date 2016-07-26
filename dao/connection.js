358b68c9b60a85e5e9a24eae58bd549d28cfd953
soulwalkerwang <soulwalkerwang@gmail.com>
2016-07-26 10:59:40 -0400

    07/25/2016

--- dao/connection.js
+++ dao/connection.js
@@ -3,12 +3,34 @@
  */
 var mysql = require('mysql');
 var dataconfig = require('../dataconn.json');
-var db = mysql.createConnection({
+var db_config ={
     host:     dataconfig.host,
     user:     dataconfig.user,
     password: dataconfig.password,
     database: dataconfig.database
-});
+};
+var db;
+function handleDisconnect() {
+    db = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+    db.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    db.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 function Post(row) {
     this.postId =  row.id;
